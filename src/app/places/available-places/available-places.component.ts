907c21773e9c54fc7a644351816e9cbfc5be5515
IAmBaconator <107735348+IAmBaconator@users.noreply.github.com>
2025-11-10 19:58:16 +0000

    Add error logic to handle http errors.

--- src/app/places/available-places/available-places.component.ts
+++ src/app/places/available-places/available-places.component.ts
@@ -4,7 +4,7 @@ import { HttpClient } from '@angular/common/http';
 import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
-import { map } from 'rxjs';
+import { catchError, map, throwError } from 'rxjs';
 
 
 @Component({
@@ -16,7 +16,8 @@ import { map } from 'rxjs';
 })
 export class AvailablePlacesComponent implements OnInit {
   places = signal<Place[] | undefined>(undefined);
-  isFetching = signal(false);
+  isFetching = signal(false); // For displaying a message to the viewer while content loads.
+  error = signal(''); // For handling any issues with the res.
   private httpClient = inject(HttpClient);
   private destroyRef = inject(DestroyRef); // Even thought the HttpClient requests tend to only return one request, it's good practise to include the DestryRef clean up the HttpClient subscripition.
 
@@ -31,7 +32,16 @@ export class AvailablePlacesComponent implements OnInit {
         //observe: 'events' // Another supported setting that will trigger for different events that occur doing the req/res lifecycle.
       })
       .pipe(
-        map((resData) => resData.places) // Not necessary, but for demo purposes.
+        map((resData) => resData.places),
+        catchError((error) => {
+          console.log(error);
+          return throwError(
+            () => 
+              new Error(
+                'Something went wrong fetching the available places. Please try again later.'
+              )
+            );
+       }) // Not necessary, but for demo purposes.
       )
       .subscribe({
         next: (places) => {
@@ -39,6 +49,9 @@ export class AvailablePlacesComponent implements OnInit {
           //console.log(event);
           //console.log(resData.places); // Modify the response to reflect the added observ parameer above and add a "?" to support undefined inititally.
           this.places.set(places);
+        },
+        error: (error: Error) => {
+          this.error.set(error.message);
         },
         complete: () => { // Safest route to use in case multiple requets need to complete before this is set.
           this.isFetching.set(false);
