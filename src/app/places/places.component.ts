f45d0c3199b8a1366592a9dd26c4bf9b4a689a85
IAmBaconator <107735348+IAmBaconator@users.noreply.github.com>
2025-11-10 19:15:30 +0000

    Host Url override of http://localhost:3000/ for Git.Codespace.

--- src/app/places/places.component.ts
+++ src/app/places/places.component.ts
@@ -12,6 +12,7 @@ import { Place } from './place.model';
 export class PlacesComponent {
   places = input.required<Place[]>();
   selectPlace = output<Place>();
+  hostUrl = 'https://congenial-goldfish-gjxqgxwq46jcwr5q-3000.app.github.dev/'; // Custom URL override of (http://localhost:3000/) for codespace that will need to be updated every time new npm start occurs.
 
   onSelectPlace(place: Place) {
     this.selectPlace.emit(place);
