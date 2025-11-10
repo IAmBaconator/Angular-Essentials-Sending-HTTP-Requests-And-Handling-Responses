09c477301eea5c7e9b209d0f581134f8c9bbf7e2
IAmBaconator <107735348+IAmBaconator@users.noreply.github.com>
2025-11-10 20:48:25 +0000

    Added hostUrl to services to make it available for places and all children components.

--- src/app/places/places.service.ts
+++ src/app/places/places.service.ts
@@ -7,6 +7,7 @@ import { Place } from './place.model';
 })
 export class PlacesService {
   private userPlaces = signal<Place[]>([]);
+  public hostUrl: string = 'https://congenial-goldfish-gjxqgxwq46jcwr5q-3000.app.github.dev/'; // Custom URL override of (http://localhost:3000/) for codespace that will need to be updated every time new npm start occurs.
 
   loadedUserPlaces = this.userPlaces.asReadonly();
 
