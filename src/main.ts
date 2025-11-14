f4cbc92deea7bc33507afc8f64570bdc4ce2b4de
IAmBaconator <107735348+IAmBaconator@users.noreply.github.com>
2025-11-14 19:50:01 +0000

    Register Interceptor Response

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,31 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { HttpHandlerFn, HttpRequest, provideHttpClient, withInterceptors } from '@angular/common/http';
+import { HttpEventType, HttpHandlerFn, HttpRequest, provideHttpClient, withInterceptors } from '@angular/common/http';
 
 import { AppComponent } from './app/app.component';
+import { tap } from 'rxjs';
 
-function loggingInterceptor(request: HttpRequest<unknown>, next: HttpHandlerFn) {
-    const req = request.clone({
-        headers: request.headers.set('X-DEBUG', 'TESTING') //To change header value. This will error in the consol because the backend won't allow it but you can look at the network tab in DevTools
-    });
+function loggingInterceptor(
+    request: HttpRequest<unknown>,
+    next: HttpHandlerFn
+) {
+    // const req = request.clone({
+    //     headers: request.headers.set('X-DEBUG', 'TESTING') //To change header value. This will error in the consol because the backend won't allow it but you can look at the network tab in DevTools
+    // });
     console.log('[Outgoing Request]');
     console.log(request);
-    return next(req);
+    //return next(req);
+    return next(request).pipe(
+        tap({ //Add reponse logging.
+            next: event => {
+                if (event.type === HttpEventType.Response) {
+                    console.log('[Incoming Response]');
+                    console.log(event.status);
+                    console.log(event.body);
+                }
+            }
+        })
+    );
+
 } //Function not class even though in older version of Angular you would set these up as classes.
 
 bootstrapApplication(AppComponent, {
