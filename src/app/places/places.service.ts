d337846b8d49956b6e92a58e3b4f885180f85a9b
IAmBaconator <107735348+IAmBaconator@users.noreply.github.com>
2025-11-14 19:32:19 +0000

    Link Error Modal & Add Delete Places.

--- src/app/places/places.service.ts
+++ src/app/places/places.service.ts
@@ -3,11 +3,13 @@ import { catchError, map, tap, throwError } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 
 import { Place } from './place.model';
+import { ErrorService } from '../shared/error.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PlacesService {
+  private errorService = inject(ErrorService);
   private httpClient = inject(HttpClient);
   private userPlaces = signal<Place[]>([]);
   hostUrl: string = 'http://automatic-fiesta-9r69p649qjg2x44q.github.dev/'; // Custom URL override of (http://localhost:3000/) for codespace that will need to be updated every time new npm start occurs.
@@ -34,21 +36,42 @@ export class PlacesService {
   addPlaceToUserPlaces(place: Place) {
     const prevPlaces = this.userPlaces();//doesn't setup a subscription, just reads the value.
 
-    if (prevPlaces.some((p) => p.id === place.id)) {//to check and ensure we do not have duplicate places added to our favorites.
-      this.userPlaces.update(prevPlaces => [...prevPlaces, place]);
+    if (!prevPlaces.some((p) => p.id === place.id)) {//to check and ensure we do not have duplicate places added to our favorites.
+      this.userPlaces.set(...prevPlaces, place);
     }
     
-    return this.httpClient.put(this.hostUrl + 'user-places', {
-      placeId: place.id,
-    }).pipe(
-      catchError(error => {
-        this.userPlaces.set(prevPlaces);//roll-back if an error takes place between the front/backend.
-        return throwError(() => new Error('Failed to store selected place.'))
+    return this.httpClient
+      .put(this.hostUrl + 'user-places', {
+        placeId: place.id,
       })
-    );
+      .pipe(
+        catchError((error) => {
+          this.userPlaces.set(prevPlaces);//roll-back if an error takes place between the front/backend.
+          this.errorService.showError('Failed to store selected place.');
+          return throwError(() => new Error('Failed to store selected place.'));
+        })
+      );
   }
 
-  removeUserPlace(place: Place) {}
+  removeUserPlace(place: Place) {
+    const prevPlaces = this.userPlaces();//doesn't setup a subscription, just reads the value.
+
+    if (prevPlaces.some((p) => p.id === place.id)) {//to check and ensure we do not have duplicate places added to our favorites.
+      this.userPlaces.set(prevPlaces.filter(p => p.id !== place.id));
+    }
+
+    return this.httpClient
+    .delete(this.hostUrl + 'user-place' + place.id)
+    .pipe(
+        catchError((error) => {
+          this.userPlaces.set(prevPlaces);
+          this.errorService.showError('Failed to remove the selected place.');
+          return throwError(
+            () => new Error('Failed to remove the selected place.')
+          );
+        })
+    );
+  }
 
   private fetchPlaces(url: string, errorMessage: string) {
     return this.httpClient.get<{places: Place[]}>(url).pipe(
